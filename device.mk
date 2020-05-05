266227e69e666f3b2257d6604b26008c90044503
Lunarixus <Nathanb644@hotmail.co.uk>
2020-05-05 13:11:44 +0000

    lavender: Remove packages at build time
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- device.mk
+++ device.mk
@@ -39,6 +39,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.device.rc
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Shims
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
