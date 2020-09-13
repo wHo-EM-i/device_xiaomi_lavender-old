94be0fad4932fced0222930eab588b22c5fa4af7
Alexander Koskovich <zvnexus@outlook.com>
2020-09-12 17:59:45 -0700

    lavender: Enable blur support in SF
     * Looks really good.
    
    Change-Id: I236564742eed7a23eaad8b16909f5ea6d81000a0
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -216,7 +216,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.max_virtual_display_dimension=4096
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
 
 # Enable Gestural navbar by default
 PRODUCT_PROPERTY_OVERRIDES += \
