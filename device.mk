e886d39e719ec12bc31bfeb02a27258e52e7aa74
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-12-14 10:19:03 +0530

    lavender: drop FM Radio
    * no use of it these days and it doesn't work
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- device.mk
+++ device.mk
@@ -200,13 +200,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio \
-    qcom.fmradio.xml
-
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
