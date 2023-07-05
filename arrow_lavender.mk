1ef47edff993bc45cd81d4987cbb9f1f2c0d2e70
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-05 13:10:04 +0530

    lavender: ArrowOS Bringup
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- arrow_lavender.mk
+++ arrow_lavender.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := arrow_lavender
 PRODUCT_MODEL := Redmi Note 7
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_GAPPS_ARCH := arm64
