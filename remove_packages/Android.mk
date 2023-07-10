eae9e892161a5870059feeb06075870ea174df43
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-07-10 11:14:18 +0530

    lavender: Remove few more google apps
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := FilesPrebuilt Maps PixelDocumentsUIGoogleOverlay PixelDocumentsUIOverlay SoundPickerPrebuilt Wellbeing Youtube
+LOCAL_OVERRIDES_PACKAGES :=  \
+    Maps Youtube TurboPrebuilt CalendarGoogle \
+    RecorderPrebuilt PrebuiltGmail Gmail2 \
+    Drive AndroidAutoStubPrebuilt AndroidAutoStub \
+    Wellbeing WellbeingPrebuilt PersonalSafety \
+    TurboAdapter SafetyHubPrebuilt arcore TagGoogle \
+    CalendarGooglePrebuilt Photos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
